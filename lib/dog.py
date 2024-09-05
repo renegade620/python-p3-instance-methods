1f36654ee534e216c715490e29d42401ee074df2
Frankline Were <franklinegift@gmail.com>
2024-09-05 09:47:54 +0300

    class and objects

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
